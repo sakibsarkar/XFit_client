c2b704a558eb2f8d00dd3dfb5273793ce7efb3b7
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-08 20:25:20 +0600

    hero section created

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
   CarouselContent,
@@ -7,30 +6,42 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import sliderImage1 from "@/assets/avengers.jpg";
-import sliderImage2 from "@/assets/justice-league-superman-banner.png";
-import sliderImage3 from "@/assets/doctor-strange.jpg";
-
+import Autoplay from "embla-carousel-autoplay";
+import { Link } from "react-router-dom";
+export const RenderNewLine = ({ text }: { text: string }) => {
+  return text.split("\n").map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+    </span>
+  ));
+};
 export function HeroSection() {
   const sliderData = [
     {
       id: 1,
-      image: sliderImage1,
+      image: "/images/slider3.webp",
+      heading: "Stylish\nHand Arm",
+      desc: "Discover top-tier fitness equipment designed to take your workouts to the next level. From cardio machines to strength training essentials, we have everything you need to achieve your fitness goals.",
     },
     {
       id: 2,
-      image: sliderImage2,
+      image: "/images/slider2.webp",
+      heading: "Elevate Your\nFitness Journey",
+      desc: "Discover top-tier fitness equipment designed to take your workouts to the next level. From cardio machines to strength training essentials, we have everything you need to achieve your fitness goals.",
     },
     {
       id: 3,
-      image: sliderImage3,
+      image: "/images/slider1.webp",
+      heading: "Gear Up for\nSuccess",
+      desc: "Discover top-tier fitness equipment designed to take your workouts to the next level. From cardio machines to strength training essentials, we have everything you need to achieve your fitness goals.",
     },
   ];
 
   return (
-    <div className="relative w-full h-[500px] mt-6">
+    <div className="relative w-full mt-6">
       <Carousel
-        className=" overflow-hidden rounded-lg shadow-lg"
+        className=" overflow-hidden rounded-lg shadow-lg h-[250px] sm:h-[300px] lg:h-[500px]"
         plugins={[
           Autoplay({
             delay: 6000,
@@ -41,12 +52,32 @@ export function HeroSection() {
           {sliderData.map((slider) => (
             <CarouselItem key={slider.id} className="min-w-full">
               <Card className="bg-transparent">
-                <CardContent className="flex items-center justify-center h-[500px] p-0">
-                  <img
-                    src={slider?.image}
-                    className="h-full w-full object-cover transition-transform duration-500 hover:scale-105"
-                    alt=""
-                  />
+                <CardContent className="flex items-center justify-center h-full p-0 w-full">
+                  <div
+                    className="relative w-full h-[250px] sm:h-[300px] lg:h-[500px] hover:scale-[1.03]"
+                    style={{ transition: "0.3s" }}
+                  >
+                    <img
+                      src={slider.image}
+                      className="absolute z-[1]  top-0 left-0 h-full w-full object-cover transition-transform duration-500 hover:scale-105"
+                      alt=""
+                    />
+
+                    <div className="relative z-10 w-full h-full flex items-start flex-col justify-center pl-[50px] bg-[#00000011] gap-[5px]">
+                      <h1 className="text-[20px] sm:text-[30px] lg:text-[60px] font-[700] text-white">
+                        <RenderNewLine text={slider.heading} />
+                      </h1>
+                      <p className="max-w-[550px] text-white text-[12px] sm:text-[14px] lg:text-[16px]">
+                        {slider.desc}
+                      </p>
+                      <Link
+                        to={"/"}
+                        className="px-[20px] border-[1px] border-white py-[5px] text-white mt-[20px]"
+                      >
+                        Shop Npw
+                      </Link>
+                    </div>
+                  </div>
                 </CardContent>
               </Card>
             </CarouselItem>
